d72ffc413f60f6c68448c033e5e7d83a16c14455
dwrth <github@dwrth.dev>
2026-07-25 16:01:15 +0200

    codecrafters submit [skip ci]

--- app/scanner.ts
+++ app/scanner.ts
@@ -122,7 +122,7 @@ export default class Scanner {
 
   this.addToken(
    TokenType.NUMBER,
-   Number.isInteger(literal) ? Number.parseInt(literal) + '.0' : literal,
+   Number.isInteger(parseFloat(literal)) ? parseInt(literal) + '.0' : literal,
   );
  }
 
